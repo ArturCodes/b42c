b80a6ddcca2568e6ca94218b55288532af9888d5
ArturCodes <arturshapiro@gmail.com>
2021-03-06 14:16:52 -0600

    found the issue (was set to private)

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
           <Route path="/footer">
             <Footer />
           </Route>
-
           <Route path="/contact">
             <Contact />
           </Route>
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,10 +1,11 @@
 import Navbar from "../Navbar/navbar"
-
+import "./home.scss"
 
 const Home = () => {
     return ( 
         <div>
             <Navbar />
+            <div className="home"></div>
         </div>
      );
 }
