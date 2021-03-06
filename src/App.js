0a644fa6584aace8808e37952b475252c05ebfc5
ArturCodes <arturshapiro@gmail.com>
2021-03-06 11:45:01 -0600

    3/6/21

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import "./app.css"
+
+import Home from "./Components/Home/home"
+import About from "./Components/About/about"
+import Contact from "./Components/Contact/contact"
+import Footer from "./Components/Footer/footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div>
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route  path="/about">
+            <About />
+          </Route>
+
+          <Route path="/footer">
+            <Footer />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route>
+          </Route>
+
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,21 @@
+import "./navbar.scss"
+
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+
+            <div className="logo">
+
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About Us</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Navbar;
