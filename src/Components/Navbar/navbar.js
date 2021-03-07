6c8fba553b2e54aec1b8812b4395f2683c486f4d
ArturCodes <arturshapiro@gmail.com>
2021-03-07 00:06:38 -0600

    this logo webpack/react issue is driving me nuts

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,19 +1,22 @@
 import "./navbar.scss"
 import { NavLink } from "react-router-dom"
+import myLogo from "../../assets/myLogo2.png"
 
 const Navbar = () => {
+    console.log({myLogo})
     return (
         <div className="navbar-wrapper">
-            <div className="logo">
-                <NavLink exact to="/"><img src="../../resouuces/logo" alt="Elite Media Design" />
-                </NavLink>
+
+            <div className="nav-left">
+                <img src={require('./myLogo.png')} alt="Elite Media Design"></img>
             </div>
-            <ul>
+
+            <div className="nav-right">
                 <NavLink exact to="/">Home </NavLink>
                 <NavLink to="/services">Services</NavLink>
                 <NavLink to="/about">About</NavLink>
                 <NavLink to="/contact">Contact</NavLink>
-            </ul>
+            </div>
         </div>
     );
 }
--- src/Components/Pages/Home/home.js
+++ src/Components/Pages/Home/home.js
@@ -1,14 +1,13 @@
 import Navbar from "../../Navbar/navbar"
 import "./home.scss"
-import ink from "../../../assets/ink.mp4"
+import ink from "../../../assets/ink.webm"
 import inkBG from "../../../assets/inkBG.png"
 
 const Home = () => {
     return (
         <div className="home-wrapper">
             <Navbar />
-            <video src={ink} autoPlay="true" loop="true" muted="true" poster={inkBG} className="bg-vid">
-            </video>
+            <video src={ink} autoPlay="true" loop="true" muted="true" poster={inkBG}></video>
         </div>
     );
 }
