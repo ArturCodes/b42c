c19c530f97026c001f3839d3741c08e3f90c6466
ArturCodes <arturshapiro@gmail.com>
2021-03-07 17:53:02 -0600

    its a mess

--- src/Components/Pages/Home/Clients/clients.js
+++ src/Components/Pages/Home/Clients/clients.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import "./clients.scss"
 
+import BestBuy from "../../../img/bb.png"
+import WallMarat from "../../../img/wm.png"
+import BlackFish from "../../../img/bf.png"
+import Clam from "../../../img/c.png"
+import FleetFarm from "../../../img/ff.png"
+
 
 function clients() {
     return (
         <div className="clients-wrapper">
-            <h1>Clients that we worked with</h1>
-            <div className="drop"></div>
+
+            <div>
+                <h1>Clients that we worked with</h1>
+            </div>
+
+            <div className="clients">
+                <img src={BestBuy} alt="BestBuy" />
+                <img src={WallMarat} alt="WallMarat" />
+                <img src={BlackFish} alt="BlackFish" />
+                <img src={Clam} alt="Clam" />
+                <img src={FleetFarm} alt="FleetFarm" />
+            </div>
+
         </div>
     )
 }
--- src/Components/Pages/Home/Footer/footer.js
+++ src/Components/Pages/Home/Footer/footer.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
 import "./footer.scss"
+import Logo from "../../../img/Logo.png"
 
 function Footer() {
     return (
         <div className="footer-wrapper">
-            FOOTER
+            <div>
+                <NavLink exact to="/">Home </NavLink>
+                <NavLink to="/services">Services</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
+            <div><img src={Logo} alt=""/></div>
+            <div>
+                
+            </div>
         </div>
     )
 }
