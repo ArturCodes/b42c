33e895dfc96c0366482832c90140dda2ae6a7fee
ArturCodes <arturshapiro@gmail.com>
2021-03-07 20:09:54 -0600

    sleep

--- src/Components/Pages/About/about.js
+++ src/Components/Pages/About/about.js
@@ -7,10 +7,14 @@ const About = () => {
         <div className="about-wrapper">
             <Navbar />
             <div className="about">
-            <h1>About Us</h1>
-                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-           
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                <h1>About Us</h1>
+                <p> EMD was founded in 2005 in a garage when two college roommates, Henry and John, put their minds together to create all things web related. The office, now located in the IDS Center in downtown Minneapolis, has over 30 employees.</p>
+
+                <h3>EMD’s Mission Statement:</h3>
+
+                <p>
+                    We strive to deliver value in our service, and to understand with respect all of our clients needs and to provide a partnership that is created when we host our clients web services, develop their site, market their product/service or simply give them advice..</p>
+
             </div>
             {/* <Footer/> */}
         </div>
